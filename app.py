8df98087d2206b91afc5d9fb727756bf11fb3139
duyle <duy.leduc8@gmail.com>
2020-11-30 16:30:15 +0700

    show rectangle and label

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request
 import imageCapturer
 import os
 
@@ -8,7 +8,8 @@ app = Flask(__name__)
 @app.route("/capture", methods=["POST"])
 def capture():
     json = request.get_json()
-    imageCapturer.imageCapturer(json['id'], 5)
+    imageCapturer.imageCapturer(
+        image_name=json['id'], wait_time=5, show_label_time=2)
     return 'Done'
 
 
--- imageCapturer.py
+++ imageCapturer.py
@@ -5,15 +5,41 @@ from pathlib import Path
 
 Path("./images").mkdir(parents=True, exist_ok=True)
 
+# * If we have a tuple of starting coordinate(5, 5) and ending coordinate(220, 220) then
+# *    (5,5)                (5, 220)
+# *      ***********************
+# *      *                     *
+# *      *                     *
+# *      *                     *
+# *      *                     *
+# *      *                     *
+# *      ***********************
+# *  (220, 5)              (220, 220)
 
-def imageCapturer(image_name, wait_time):
+
+def imageCapturer(image_name, wait_time, show_label_time):
     cap = cv2.VideoCapture(0)
     ts = int(time())
+    label_time = datetime.now() + timedelta(seconds=show_label_time)
     finish_time = datetime.now() + timedelta(seconds=wait_time)
 
     try:
         while cap.isOpened():
             ret, img = cap.read()
+
+            height, width, channel = img.shape
+            center_width = width // 2
+            center_height = height // 2
+            starting_point = (center_width - 100, center_height - 150)
+            ending_point = (center_width + 100, center_height + 150)
+            text_org = (center_width - 80, center_height - 170)
+
+            if(datetime.now() > label_time):
+                cv2.rectangle(img, starting_point,
+                              ending_point, (0, 255, 0), 2)
+                cv2.putText(img, 'Black bean plant', text_org,
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+
             cv2.imshow("My webcam", img)
 
             if not ret:
