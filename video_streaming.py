0ad0aa840e6cd352dbf662688603d3069e4f8598
duyle <duy.leduc8@gmail.com>
2020-12-25 10:47:03 +0700

    prepare for predict with opencv

--- video_streaming.py
+++ video_streaming.py
@@ -9,10 +9,14 @@ class VideoStreaming:
         self.__cap = cv2.VideoCapture(0)
 
     def stream(self):
-        model = load_model('training_models\\apple_leaves_diseases_model.h5')
+        model = load_model(r'training_models/apple_leaves_diseases_model.h5')
+        class_labels = ['healthy', 'rust', 'scab']
 
         while True:
             ret, frame = self.__cap.read()
+
+            cv2.imshow('frame', frame)
+            print(type(frame)) #numpy array
 
             if not ret:
                 print("Error: failed to capture image")
@@ -20,9 +24,14 @@ class VideoStreaming:
 
             # TODO: Make a Prediction here
 
-            cv2.imwrite('frame.jpg', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+            #cv2.imwrite('frame.jpg', frame)
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + open('frame.jpg', 'rb').read() + b'\r\n')
+
+        self.__cap.release()
+        cv2.destroyAllWindows()
 
     def close(self, signal_received, frame):
         self.__cap.release()
