4252f68e3229a7623aff6825d4d0352343caf7dc
duyle <duy.leduc8@gmail.com>
2020-12-29 16:21:36 +0700

    separate predict and stream functionalities

---
+++ predict.py
@@ -0,0 +1,24 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications import imagenet_utils
+
+model = load_model(r'models/apple_leaves_diseases_model.h5')
+
+
+def prepare_image(file_name):
+    img_path = 'images/'
+    img = image.load_img(img_path + file_name, target_size=(224, 224))
+    img_array = image.img_to_array(img)
+    img_array_expanded_dims = np.expand_dims(img_array, axis=0)
+    return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
+
+
+def predict(file_name):
+    image = prepare_image(file_name)
+    predictions = model.predict(image)
+    # * Get top 3 of predictions
+    results = imagenet_utils.decode_predictions(predictions, top=3)
+    print(results)
--- video_streaming.py
+++ video_streaming.py
@@ -1,7 +1,9 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-import numpy as np
 import cv2
+import os
+from predict import predict
+
+if not os.path.exists('images'):
+    os.makedirs('images')
 
 
 class VideoStreaming:
@@ -10,51 +12,26 @@ class VideoStreaming:
         self.__cap = cv2.VideoCapture(0)
 
     def stream(self):
-        leaves_classifier = cv2.CascadeClassifier(
-            r'classifiers/cascade.xml')
-        model = load_model(r'training_models/apple_leaves_diseases_model.h5')
-
         while True:
             ret, frame = self.__cap.read()
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            leaves_detected = leaves_classifier.detectMultiScale(gray, 1.32, 5)
-            class_labels = ['healthy', 'rust', 'scab']
-
-            print(leaves_detected)
-            # print(type(frame))  # numpy array
 
             if not ret:
                 print("Error: failed to capture image")
                 break
 
-            # TODO: Make a Prediction here
-            for (x, y, w, h) in leaves_detected:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_gray = cv2.resize(roi_gray, (48, 48),
-                                      interpolation=cv2.INTER_AREA)
-
-                if np.sum([roi_gray]) != 0:
-                    roi = roi_gray.astype('float')/255.0
-                    roi = image.img_to_array(roi)
-                    roi = np.expand_dims(roi, axis=0)
-
-                    preds = model.predict(roi)[0]
-                    label = class_labels[preds.argmax()]
-                    label_position = (x, y)
-                    cv2.putText(img, label, label_position,
-                                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-                else:
-                    cv2.putText(img, 'No Leaf Found', (20, 60),
-                                cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-
             cv2.imshow('frame', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-            #cv2.imwrite('frame.jpg', frame)
+
+            # ? Save file in specific dir
+            file_name = 'frame.jpg'
+            os.chdir('images')
+            cv2.imwrite(file_name, frame)
+            os.chdir('../')
             # yield (b'--frame\r\n'
             #        b'Content-Type: image/jpeg\r\n\r\n' + open('frame.jpg', 'rb').read() + b'\r\n')
+        predict(file_name)
 
         self.__cap.release()
         cv2.destroyAllWindows()
