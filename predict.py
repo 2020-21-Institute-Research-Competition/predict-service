e9a692e3b666b9812716a247aebde8d4366d4d04
duyle <duy.leduc8@gmail.com>
2020-12-30 00:40:13 +0700

    predict image after saved

--- predict.py
+++ predict.py
@@ -2,22 +2,39 @@ import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
-from tensorflow.keras.applications import imagenet_utils
+from tensorflow.keras.utils import get_file
+import json
+
+CLASS_INDEX = None
+CLASS_INDEX_PATH = 'class_index.json'
+
+
+def decode_predictions_modified(preds, top=1):
+    global CLASS_INDEX
+    if len(preds.shape) != 2 or preds.shape[1] != 3:
+        raise ValueError(
+            '`decode_predictions` expects ' 'a batch of predictions ''(i.e. a 2D array of shape (samples, 1000)). ' 'Found array with shape: ' + str(preds.shape))
+    if CLASS_INDEX is None:
+        CLASS_INDEX = json.load(open(CLASS_INDEX_PATH))
+    results = ''
+    for pred in preds:
+        top_indices = pred.argsort()[-top:][::-1]
+        for i in top_indices:
+            # print(pred[i])
+            results = CLASS_INDEX[str(i)]
+    return results
 
 
 def prepare_image(file_name):
-    img_path = 'images/'
+    img_path = 'images\\'
     img = image.load_img(img_path + file_name, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array_expanded_dims = np.expand_dims(img_array, axis=0)
     return tf.keras.applications.mobilenet.preprocess_input(img_array_expanded_dims)
 
 
-def predict(file_name):
-    model = load_model(r'models/apple_leaves_diseases_model.h5')
+def predict(file_name, model):
     image = prepare_image(file_name)
     predictions = model.predict(image)
-    # * Get top 3 of predictions
-    results = imagenet_utils.decode_predictions(predictions, top=3)
+    results = decode_predictions_modified(predictions, top=1)
     print(results)
--- video_streaming.py
+++ video_streaming.py
@@ -1,17 +1,18 @@
 import cv2
 import os
+from tensorflow.keras.models import load_model
 from predict import predict
-
-if not os.path.exists('images'):
-    os.makedirs('images')
 
 
 class VideoStreaming:
     def __init__(self):
         super().__init__()
         self.__cap = cv2.VideoCapture(0)
+        if not os.path.exists('images'):
+            os.makedirs('images')
 
     def stream(self):
+        model = load_model(r'models/apple_leaves_diseases_model.h5')
         while True:
             ret, frame = self.__cap.read()
 
@@ -22,19 +23,16 @@ class VideoStreaming:
             cv2.imshow('frame', frame)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
+                # ? Save file in specific dir
+                file_name = 'frame.jpg'
+                cv2.imwrite(f'images\\{file_name}', frame)
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + open('images\\frame.jpg', 'rb').read() + b'\r\n')
                 break
-
-            # ? Save file in specific dir
-            file_name = 'frame.jpg'
-            os.chdir('images')
-            cv2.imwrite(file_name, frame)
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + open('frame.jpg', 'rb').read() + b'\r\n')
-        predict(file_name)
-        os.chdir('../')
-
         self.__cap.release()
         cv2.destroyAllWindows()
+
+        predict(file_name, model)
 
     def close(self, signal_received, frame):
         self.__cap.release()
