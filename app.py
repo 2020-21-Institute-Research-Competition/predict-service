abe53549edd6a279470c69aac3b55ed6f89be2c1
Vu Huy <vuhuycto@gmail.com>
2021-01-04 16:06:29 +0700

    fix session bug

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def latest():
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, streaming.close)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=False, threaded=False)
